f41894b3f7a5b72cde351f032dadd46fb669e6da
zhangtianyang <zhangty@healthmall.cn>
2018-07-18 15:47:40 +0800

    commit source code

---
+++ mylibs/src/main/java/com/example/mylibs/MyApplication.java
@@ -0,0 +1,15 @@
+package com.example.mylibs;
+
+import android.app.Application;
+
+/**
+ * Create by Zhangty on 2018/7/18
+ */
+public class MyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //TODO 在这里可以做一些一个App必须做的初始化操作,比如初始化网络缓存框架，登录操作等等，自己根据自己的项目来走
+    }
+}
